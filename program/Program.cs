7a0b9be3b4e40fbc4fb6f48e1bcccd0ad4d20457
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-05-05 16:19:06 +0400

    Добавлена функция для фильтрации и создания нового массива

--- program/Program.cs
+++ program/Program.cs
@@ -4,3 +4,35 @@
 Console.WriteLine("Введите элементы массива через пробел:");
 string[] array = Console.ReadLine().Split(' ');
 
+// Функция формирования нового массива
+string[] NewArray(string[] inputArray)
+{
+    // Счетчик элементов массива
+    int count = 0;
+
+    // Цикл подсчета элементов массива
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3) count++;
+    }
+
+    // Создание нового массива размером count
+    string[] resultArray = new string[count];
+
+    // Счетчик для нового массива
+    int j = 0;
+
+    // Цикл заполнения нового массива
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            // Заполнение нового массива
+            resultArray[j] = inputArray[i];
+            j++;
+        }
+    }
+    // Возврат нового массива
+    return resultArray;
+}
+
